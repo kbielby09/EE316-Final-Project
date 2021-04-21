28488d45609ce90fddbe6dd08c6d23f4d39a7437
kbielby09 <kbielby09@gmail.com>
2021-04-21 14:02:46 -0400

    added hello world test

---
+++ Project6_test/Project6_test/Project6_test.sdk/vga_test2/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../vga_test2_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../vga_test2_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../vga_test2_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../vga_test2_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../vga_test2_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../vga_test2_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../vga_test2_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../vga_test2_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
